40d558023feb8f343f243207a45addbcde1097e1
Unknown <petter.f.andersson@gmail.com>
2019-01-09 15:41:41 +0100

    Add background gradient

--- src/components/molecules/total/Total.template.js
+++ src/components/molecules/total/Total.template.js
@@ -11,7 +11,7 @@ function getExpensesTotal() {
   return `
     <div class="m-total">
       <p class="m-total__title">Total:</p>
-      <p class="m-total__sum m-total__sum--expenses">9 500</p>
+      <p class="m-total__sum m-total__sum--expenses">- 9 500</p>
     </div>
   `;
 }
