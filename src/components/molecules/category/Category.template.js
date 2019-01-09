55040206e596484677a8e897996554a7f76a73f8
Unknown <petter.f.andersson@gmail.com>
2019-01-09 13:55:05 +0100

    Style Category component

--- src/components/molecules/category/Category.template.js
+++ src/components/molecules/category/Category.template.js
@@ -1,11 +1,11 @@
 function getIncomeCategories() {
   return this.incomeCategoryNames.map(name => {
     return `
-      <div class="input-group mb-3 d-flex">
-        <div class="input-group-prepend w-75">
-          <span class="input-group-text w-100" id="inputGroup-sizing-default">${name}</span>
-        </div>
-        <input type="text" class="form-control text-right" placeholder="kr" aria-label="Sum input" aria-describedby="inputGroup-sizing-default">
+      <div class="m-category">
+        <p class="m-category__title">
+          ${name}
+        </p>
+        <input type="text" class="m-category__input form-control text-right" placeholder="$" aria-label="Sum input">
       </div>
     `;
   }).join('');
