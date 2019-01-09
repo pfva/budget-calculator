ae29630111059ee5ba8b41c942707d516d9eff2b
Unknown <petter.f.andersson@gmail.com>
2019-01-09 14:21:29 +0100

    Style Total component

--- src/components/molecules/total/Total.template.js
+++ src/components/molecules/total/Total.template.js
@@ -1,10 +1,8 @@
 export default function() {
   return `
-    <div class="input-group mb-3 d-flex">
-      <div class="input-group-prepend w-75">
-        <p class="font-weight-bold w-100 text-right pr-2 pt-2">TOTAL:</p>
-      </div>
-      <input type="text" class="form-control text-right" placeholder="kr"  aria-label="Sum input" aria-describedby="inputGroup-sizing-default" disabled>
+    <div class="m-total">
+      <p class="m-total__title">Total:</p>
+      <p class="m-total__sum m-total__sum--income">13 500</p>
     </div>
   `;
 }
