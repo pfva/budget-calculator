5acc07235b3b71a8b2d3d4603aa10ae150d5de1e
Unknown <petter.f.andersson@gmail.com>
2019-01-11 16:07:18 +0100

    Put default value in Total boxes to avoid NaN issues

--- src/components/molecules/total/Total.template.js
+++ src/components/molecules/total/Total.template.js
@@ -2,7 +2,7 @@ function getIncomeTotal() {
   return `
     <div class="m-total">
       <p class="m-total__title">Total:</p>
-      <p class="m-total__sum m-total__sum--income"></p>
+      <p class="m-total__sum m-total__sum--income">$ 0</p>
     </div>
   `;
 }
@@ -11,7 +11,7 @@ function getExpensesTotal() {
   return `
     <div class="m-total">
       <p class="m-total__title">Total:</p>
-      <p class="m-total__sum m-total__sum--expenses"></p>
+      <p class="m-total__sum m-total__sum--expenses">$ 0</p>
     </div>
   `;
 }
