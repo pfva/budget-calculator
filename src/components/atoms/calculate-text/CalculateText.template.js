61ecf17d4bf17711c0c1ee631a1e85da76edaf10
Unknown <petter.f.andersson@gmail.com>
2019-01-11 13:10:47 +0100

    Style money sum in CalculateText

--- src/components/atoms/calculate-text/CalculateText.template.js
+++ src/components/atoms/calculate-text/CalculateText.template.js
@@ -1,7 +1,7 @@
 export default function() {
   return `
   <div class="a-calculatetext u-hidden">
-    <p class="a-calculatetext__main">You have $ 1 350 left each month</p>
+    <p class="a-calculatetext__main">You have <span class="a-calculatetext__main--sum-positive">$ 1 350</span> left each month</p>
     <p class="a-calculatetext__subtext">Good job, keep it up! <span class="a-calculatetext__subtext--emoji">👍</span></p>
   </div>
   `;
