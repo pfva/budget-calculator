34406ed2cb180cb7672b71d69b20eff44b8743e5
Unknown <petter.f.andersson@gmail.com>
2019-01-14 13:41:03 +0100

    Add space-in-string functionality to CalculateText as well

--- src/components/atoms/calculate-text/CalculateText.template.js
+++ src/components/atoms/calculate-text/CalculateText.template.js
@@ -1,11 +1,11 @@
 export default function() {
   return `
   <div class="a-calculatetext a-calculatetext-positive u-hidden">
-    <p class="a-calculatetext__main">You have <span class="a-calculatetext__main--sum-positive">$ 1 350</span> left each month</p>
+    <p class="a-calculatetext__main">You have <span class="a-calculatetext__main--sum-positive"></span> left each month</p>
     <p class="a-calculatetext__subtext">Good job, keep it up! <span class="a-calculatetext__subtext--emoji">🎉</span></p>
   </div>
   <div class="a-calculatetext a-calculatetext-negative u-hidden">
-    <p class="a-calculatetext__main">You're spending <span class="a-calculatetext__main--sum-negative">$ -2 500</span> more than you earn each month</p>
+    <p class="a-calculatetext__main">You're spending <span class="a-calculatetext__main--sum-negative"></span> more than you earn each month</p>
     <p class="a-calculatetext__subtext">Try to lower your expenses! <span class="a-calculatetext__subtext--emoji">💸</span></p>
   </div>
   `;
--- src/components/organisms/calculate/Calculate.class.js
+++ src/components/organisms/calculate/Calculate.class.js
@@ -44,18 +44,26 @@ export default class Calculate extends Base {
       let expensesWithoutSpaces = expensesWithoutDollar.replace(/ /g, '');
       let expensesNumber = parseInt(expensesWithoutSpaces);
 
+      let totalSum = incomeNumber - expensesNumber;
+      console.log(totalSum);
+      let totalStr = totalSum.toString();
+      console.log(totalStr);
+      if(totalStr.length >= 4 && totalStr.length <= 6) {
+        totalStr = totalStr.substring(0, totalStr.length - 3) + " " + totalStr.substring(totalStr.length - 3);
+      } else if(totalStr.length >= 7 && totalStr.length <= 9) {
+        totalStr = totalStr.substring(0, totalStr.length - 6) + " " + totalStr.substring(totalStr.length - 6, totalStr.length - 3) + " " + totalStr.substring(totalStr.length - 3);
+      }
+
       // Display correct part of template
       if(incomeNumber >= expensesNumber) {
-        console.log("over");
         let positiveSum = document.querySelector('.a-calculatetext__main--sum-positive');
-        positiveSum.innerHTML = "$ " + (incomeNumber - expensesNumber);
+        positiveSum.innerHTML = "$ " + totalStr;
         let calculateText = document.querySelector('.a-calculatetext-positive');
         calculateText.classList.remove('u-hidden');
         calculateText.classList.add('u-fadein');
       } else {
-        console.log("under");
         let negativeSum = document.querySelector('.a-calculatetext__main--sum-negative');
-        negativeSum.innerHTML = "$ " + (incomeNumber - expensesNumber);
+        negativeSum.innerHTML = "$ " + totalStr;
         let calculateText = document.querySelector('.a-calculatetext-negative');
         calculateText.classList.remove('u-hidden');
         calculateText.classList.add('u-fadein');
