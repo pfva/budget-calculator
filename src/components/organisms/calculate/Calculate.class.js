7e422671d95b6cd711b59ef4c7278748448d1bdb
Unknown <petter.f.andersson@gmail.com>
2019-01-18 14:47:46 +0100

    Refactor other part of displayCalculateText(), break out to own method

--- src/components/organisms/calculate/Calculate.class.js
+++ src/components/organisms/calculate/Calculate.class.js
@@ -36,6 +36,24 @@ export default class Calculate extends Base {
     return string;
   }
 
+  addSpaceToEveryThousand(number) {
+    let str = number.toString();
+    if(number >= 0) {
+      if(str.length >= 4 && str.length <= 6) {
+        str = str.substring(0, str.length - 3) + " " + str.substring(str.length - 3);
+      } else if(str.length >= 7 && str.length <= 9) {
+        str = str.substring(0, str.length - 6) + " " + str.substring(str.length - 6, str.length - 3) + " " + str.substring(str.length - 3);
+      }
+    } else if(number < 0) {
+      if(str.length >= 5 && str.length <= 7) {
+        str = str.substring(0, str.length - 3) + " " + str.substring(str.length - 3);
+      } else if(str.length >= 8 && str.length <= 10) {
+        str = str.substring(0, str.length - 6) + " " + str.substring(str.length - 6, str.length - 3) + " " + str.substring(str.length - 3);
+      }
+    }
+    return str;
+  }
+
   displayCalculateText() {
     let calculateButton = document.querySelector('.a-button');
     calculateButton.addEventListener("click", () => {
@@ -46,34 +64,19 @@ export default class Calculate extends Base {
       let expensesNumber = this.cleanupText(expensesTotal);
 
       let totalSum = incomeNumber - expensesNumber;
-      let totalStr = totalSum.toString();
-      if(totalSum >= 0) {
-        if(totalStr.length >= 4 && totalStr.length <= 6) {
-          totalStr = totalStr.substring(0, totalStr.length - 3) + " " + totalStr.substring(totalStr.length - 3);
-        } else if(totalStr.length >= 7 && totalStr.length <= 9) {
-          totalStr = totalStr.substring(0, totalStr.length - 6) + " " + totalStr.substring(totalStr.length - 6, totalStr.length - 3) + " " + totalStr.substring(totalStr.length - 3);
-        }
-      } else if(totalSum < 0) {
-        if(totalStr.length >= 5 && totalStr.length <= 7) {
-          totalStr = totalStr.substring(0, totalStr.length - 3) + " " + totalStr.substring(totalStr.length - 3);
-        } else if(totalStr.length >= 8 && totalStr.length <= 10) {
-          totalStr = totalStr.substring(0, totalStr.length - 6) + " " + totalStr.substring(totalStr.length - 6, totalStr.length - 3) + " " + totalStr.substring(totalStr.length - 3);
-        }
-      }
+      let totalString = this.addSpaceToEveryThousand(totalSum);
 
-      // Display correct part of template
       let positiveCalculateText = document.querySelector('.a-calculatetext-positive');
       let negativeCalculateText = document.querySelector('.a-calculatetext-negative');
-
       if(incomeNumber >= expensesNumber) {
         let positiveSum = document.querySelector('.a-calculatetext__main--sum-positive');
-        positiveSum.innerHTML = "$ " + totalStr;
+        positiveSum.innerHTML = "$ " + totalString;
         negativeCalculateText.classList.add('u-hidden');
         positiveCalculateText.classList.remove('u-hidden');
         positiveCalculateText.classList.add('u-fadein');
       } else {
         let negativeSum = document.querySelector('.a-calculatetext__main--sum-negative');
-        negativeSum.innerHTML = "$ " + totalStr;
+        negativeSum.innerHTML = "$ " + totalString;
         positiveCalculateText.classList.add('u-hidden');
         negativeCalculateText.classList.remove('u-hidden');
         negativeCalculateText.classList.add('u-fadein');
