0592135e4e5464388b327a74b98315dd7b745edf
Unknown <petter.f.andersson@gmail.com>
2019-01-18 11:36:37 +0100

    Add title to chart

--- src/components/atoms/calculate-text/CalculateText.template.js
+++ src/components/atoms/calculate-text/CalculateText.template.js
@@ -3,24 +3,12 @@ export default function() {
   <div class="a-calculatetext a-calculatetext-positive u-hidden">
     <p class="a-calculatetext__main">You have <span class="a-calculatetext__main--sum-positive"></span> left each month</p>
     <p class="a-calculatetext__subtext">Good job, keep it up! <span class="a-calculatetext__subtext--emoji">🎉</span></p>
+    <p class="a-calculatetext__chart-intro">Here's how your different expenses compare to each other:</p>
   </div>
   <div class="a-calculatetext a-calculatetext-negative u-hidden">
     <p class="a-calculatetext__main">You're spending <span class="a-calculatetext__main--sum-negative"></span> more than you earn each month</p>
     <p class="a-calculatetext__subtext">Try to lower your expenses! <span class="a-calculatetext__subtext--emoji">💸</span></p>
+    <p class="a-calculatetext__chart-intro">Here's how your different expenses compare to each other:</p>
   </div>
   `;
 }
-
-// function negativeSum() {
-//   return `
-//   <div class="a-calculatetext u-hidden">
-//     <p class="a-calculatetext__main">You're spending <span class="a-calculatetext__main--sum-negative">$ -2 500</span> over your budget each month</p>
-//     <p class="a-calculatetext__subtext">Try to lower your expenses! <span class="a-calculatetext__subtext--emoji">💸</span></p>
-//   </div>
-//   `;
-// }
-
-// export {
-//   positiveSum, 
-//   negativeSum
-// }
